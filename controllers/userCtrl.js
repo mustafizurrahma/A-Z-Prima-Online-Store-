3b6b71f5dc8e5e5769663220ce59f5b5aa79ffd2
mustafizurrahma <mdmustafijur425@gmail.com>
2021-06-21 16:31:33 +0600

    added'

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -1,5 +1,6 @@
 const Users = require('../models/userModel')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 const userCtrl = {
     register: async (req, res)=>{
         try {
@@ -17,21 +18,93 @@ const userCtrl = {
                 name, email, password: passwordHash
             })
              // User Login And Register Info Save To MomgoDB
-
              await newUser.save()
 
-            res.json({newUser})
+             //Create jsonwebtoken authentication
+             const accesstoken = createAccessToken({id: newUser._id})
+             const refreshtoken = createRefreshToken({id: newUser._id})
 
-            // res.json({password, passwordHash})
+            res.cookie('refreshtoken', refreshtoken, {
+                httpOnly: true,
+                path: '/user/refresh_token'
+            })
+            // res.json({newUser})
+            res.json({accesstoken})
 
-            res.json({msg: "Register Successfully Complete"})
+            // res.json({msg: "Register Successfully Complete"})
 
         } catch (err) {
             return res.status(500).json({msg: err.message})
 
         }
+    },
+    login: async (req, res) => {
+        try {
+            const {email, password} = req.body;
+
+            const user= await Users.findOne({email})
+            if(!user) return res.status(404).json({msg: "User Dose Not Exists."})
+            const isMatch = await bcrypt.compare(password, user.password)
+            if(!isMatch) return res.status(400).json({msg: "InCorrect Your Password"})
+            //If your login success create token and Refresh Token 
+            const accesstoken = createAccessToken({id: user._id})
+            const refreshtoken = createRefreshToken({id: user._id})
+
+           res.cookie('refreshtoken', refreshtoken, {
+               httpOnly: true,
+               path: '/user/refresh_token'
+           })
+           // res.json({newUser})
+           res.json({accesstoken})
+
+        } catch (err) {
+            return res.status(500).json({msg: err.message})
+        }
+    },
+    logout: async (req, res) => {
+        try {
+            res.clearCookie('refreshtoken', {path: '/user/refresh_token'})
+            return res.json({msg:"You Are Logout"})
+        } catch (err) {
+            return res.status(500).json({msg: err.message})
+        }
+    },
+    refreshToken: (req, res) => {
+        try {
+            const rf_token = req.cookies.refreshtoken;
+
+        if(!rf_token) return res.status(400).json({msg: "Please Login Or Register"})
+
+        jwt.verify(rf_token, process.env.REFRESH_TOKEN_SECRET, (err , user) => {
+            if(err) return res.status(400).json({msg: "Please Login Or Register"})
+            const accesstoken = createAccessToken({id: user.id})
+            res.json({accesstoken})
+        })
+
+        // res.json({rf_token})
+
+        } catch (error) {
+            return res.status(500).json({msg: err.message})
+        }
+    },
+    getUser: async (req, res) => {
+        try {
+            const user = await (await Users.findById(req.user.id)).select('-password')
+            if(!user) return res.status(400).json({msg: "User Dose Not Exists."})
+            res.json(user)
+        } catch (err) {
+            return res.status(500).json({msg: err.message})
+        }
     }
     
+}
+
+const createAccessToken = (user) =>{
+    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1d'})
+}
+
+const createRefreshToken = (user) =>{
+    return jwt.sign(user, process.env.REFRESH_TOKEN_SECRET, {expiresIn: '7d'})
 }
 
 module.exports = userCtrl
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,5 +1,10 @@
 const router = require('express').Router()
 const useCtrl = require('../controllers/userCtrl')
+const auth = require('../middleware/auth')
 router.post('/register', useCtrl.register)
+router.post('/login', useCtrl.login)
+router.get('/logout', useCtrl.logout)
+router.get('/refresh_token', useCtrl.refreshToken)
+router.get('/infor', auth, useCtrl.getUser)
 
 module.exports = router
